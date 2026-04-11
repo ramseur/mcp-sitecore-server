058a403e08a93d36f85810cb47808df55e6ce4d6
Antonytm <antontishchenko@gmail.com>
2026-04-11 22:16:19 +0300

    ci: fix failed build

--- tests/client.ts
+++ tests/client.ts
@@ -16,7 +16,7 @@ const transportOptions: TransportOptions = {
 
 const client = new Client({
     name: "mcp-sitecore-server",
-    version: "1.3.7",
+    version: "1.3.8",
 });
 
 const transport = createTransport(transportOptions);
